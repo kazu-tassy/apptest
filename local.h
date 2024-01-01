174c0cd4146816a2483c3f4a021fb168b481a3f3
k-tashima <k-tashima@leo.bbiq.jp>
2024-01-01 18:58:29 +0900

    first commit

---
+++ local.h
@@ -0,0 +1,6 @@
+#define _LOCAL_H_
+#ifdef _LOCAL_H_
+
+int add(int, int);
+
+#endif
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "local.h"
+
+int main(void)
+{
+    int val;
+    val = add(1, 1);
+    printf("ans=%d", val);
+}
